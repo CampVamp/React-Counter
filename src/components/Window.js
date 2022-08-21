9e3cb97dbbd735de56f91f27d3f49e6a87eb9273
Ajay Ram <ajayramsaravanan4@gmail.com>
2022-08-21 14:10:18 +0530

    Added Reset Function

--- src/components/Window.js
+++ src/components/Window.js
@@ -3,7 +3,10 @@ import { useState } from "react"
 const Window = ({title}) => {
     const [n,setn] = useState(1)
     function increment(){
-        setn(n+1)
+        setn(prevn=>prevn+1)
+    }
+    function reset(){
+        setn(prevn=>prevn=0)
     }
     return (
     <div className="window">
@@ -11,9 +14,9 @@ const Window = ({title}) => {
             <h2>{title}</h2>
         </div>
         <div className="mainframe">
-            <h3>{n}</h3>
+            <h3 className="counter">{n}</h3>
             <button className="btn" onClick={increment}>Add</button>
-            <button className="btn">Reset</button>
+            <button className="btn" onClick={reset}>Reset</button>
         </div>
     </div>
   )

05fd13fb7eb971e6ecb465988e9e06e6348c9e59
Ajay Ram <ajayramsaravanan4@gmail.com>
2022-08-21 13:31:15 +0530

    Added Counter Function

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,14 +1,18 @@
 import { useState } from "react"
 
 const Window = ({title}) => {
+    const [n,setn] = useState(1)
+    function increment(){
+        setn(n+1)
+    }
     return (
     <div className="window">
         <div className="topbar">
             <h2>{title}</h2>
         </div>
         <div className="mainframe">
-            <h3>1</h3>
-            <button>Add</button>
+            <h3>{n}</h3>
+            <button onClick={increment}>Add</button>
         </div>
     </div>
   )
